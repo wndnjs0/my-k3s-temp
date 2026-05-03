4b8bc4142007b7536bbdd7980ac7abc00fd901e2
Juwon <juuuwon2@gmail.com>
2026-05-03 04:02:17 +0000

    .

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -9,21 +9,5 @@ helm upgrade -i argocd argo/argo-cd \
   -f values-argocd.yaml \
   --version 9.4.17
 
-
-# Apps 설치 (Secret X)
-kubectl apply -f ../apps/traefik.yaml
-kubectl apply -f ../apps/sealed-secrets.yaml
-
-# Sealed Secrets 업데이트
-kubeseal -f ../manifests/postgres/secret.yaml --controller-namespace kube-system --controller-name sealed-secrets --format yaml -w ../manifests/postgres/sealed-secret.yaml
-kubeseal -f ../manifests/memos/secret.yaml --controller-namespace kube-system --controller-name sealed-secrets --format yaml -w ../manifests/memos/sealed-secret.yaml
-kubeseal -f ../manifests/mysql/secret.yaml --controller-namespace kube-system --controller-name sealed-secrets --format yaml -w ../manifests/mysql/sealed-secret.yaml
-kubeseal -f ../manifests/ghost/secret.yaml --controller-namespace kube-system --controller-name sealed-secrets --format yaml -w ../manifests/ghost/sealed-secret.yaml
-kubeseal -f ../manifests/xray/secret.yaml --controller-namespace kube-system --controller-name sealed-secrets --format yaml -w ../manifests/xray/sealed-secret.yaml
-
-# Apps 설치
-kubectl apply -f ../apps/postgres.yaml
-kubectl apply -f ../apps/memos.yaml
-kubectl apply -f ../apps/mysql.yaml
-kubectl apply -f ../apps/ghost.yaml
-kubectl apply -f ../apps/xray.yaml
+# App of Apps 설치
+kubectl apply -f ../app-of-apps.yaml
